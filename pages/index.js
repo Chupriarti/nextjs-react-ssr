4eee2388c33f5d758168ae175d22bf110940829d
Chupriarti <wallx13@gmail.com>
2022-02-03 22:49:07 +0300

    prepare navigation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
+import Link from "next/link"
+
 const Index = () => {
     return (
         <div>
+            <div>
+                <Link href="/">
+                    <a>Main page</a>
+                </Link>
+                <Link href="/users">
+                    <a>Users</a>
+                </Link>
+            </div>
             <h1>Main page</h1>
         </div>
     )
