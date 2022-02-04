ae5154081d7102f9f2459eb61dc991f56c2cc193
Chupriarti <wallx13@gmail.com>
2022-02-04 23:18:25 +0300

    add user page with dynamic id

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,8 +1,11 @@
+import { useRouter } from "next/router"
 export default function () {
-
+    const {query} = useRouter()
     return (
         <div>
-            User
+            <h1>
+                User with id {query.id}
+            </h1>     
         </div>
     )
 }
