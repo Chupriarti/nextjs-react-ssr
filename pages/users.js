e2dad65123a8ca6b41cab721fc5bf6ab96210a40
Chupriarti <wallx13@gmail.com>
2022-02-04 23:07:02 +0300

    prepare mock users

--- pages/users.js
+++ pages/users.js
@@ -1,7 +1,22 @@
+import React from "react"
+
 const Users = () => {
+    const [users, setUsers] = React.useState([
+        {id: 1, name: 'alex'},
+        {id: 2, name: 'maxim'}
+    ])
     return (
         <div>
             <h1>Users page</h1>
+            <ul>
+                {users.map(user => 
+                    <li
+                        key={user.id}
+                    >
+                        {user.name}
+                    </li>    
+                )}
+            </ul>
         </div>
     )
 }
