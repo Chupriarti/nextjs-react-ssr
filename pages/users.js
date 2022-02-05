b801be8add26118b5cb02110bde2eb7508854bc7
Chupriarti <wallx13@gmail.com>
2022-02-05 23:39:26 +0300

    add fetching mock users api

--- pages/users.js
+++ pages/users.js
@@ -6,6 +6,13 @@ const Users = () => {
         {id: 1, name: 'alex'},
         {id: 2, name: 'maxim'}
     ])
+
+    React.useEffect(async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        setUsers(data)
+    }, [])
+
     return (
         <div>
             <h1>Users page</h1>
