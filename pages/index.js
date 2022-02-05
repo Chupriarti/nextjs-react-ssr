5e96faf90fb321d0356c2094c20410fb3b8e8a35
Chupriarti <wallx13@gmail.com>
2022-02-05 23:54:02 +0300

    add seo data

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,30 @@
+import Head from "next/head"
 import Link from "next/link"
 import A from "../components/A"
 
 const Index = () => {
     return (
-        <div>
-            <div className="navbar">
-                <A href={"/"} text={"Main page"} />
-                <A href={"/users"} text={"Users"} />
+        <>
+            <Head>
+                <meta keywords="testing nextjs"></meta>
+                <title>Main page</title>
+            </Head>
+            <div>
+                <div className="navbar">
+                    <A href={"/"} text={"Main page"} />
+                    <A href={"/users"} text={"Users"} />
+                </div>
+                <h1>Main page</h1>
+                <style jsx>
+                    {`
+                        .navbar {
+                            background: orange;
+                            padding: 15px;
+                        }
+                    `}
+                </style>
             </div>
-            <h1>Main page</h1>
-            <style jsx>
-                {`
-                    .navbar {
-                        background: orange;
-                        padding: 15px;
-                    }
-                `}
-            </style>
-        </div>
+        </>
     )
 }
 
