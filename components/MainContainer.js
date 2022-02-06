41c927cd72f3683001ea8e36221e746923bca4f7
Chupriarti <wallx13@gmail.com>
2022-02-06 23:37:49 +0300

    move meta to container

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -1,8 +1,13 @@
+import Head from "next/head"
 import A from "./A"
 
-const MainContainer = ({children}) => {
+const MainContainer = ({children, keywords}) => {
     return (
         <>
+            <Head>
+                <meta keywords={"testing nextjs" + keywords}></meta>
+                <title>Main page</title>
+            </Head>
             <div className="navbar">
                 <A href={"/"} text={"Main page"} />
                 <A href={"/users"} text={"Users"} />
